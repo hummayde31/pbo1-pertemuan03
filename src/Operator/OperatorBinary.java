85f6080cd4db02376c91ce058ae954fa398b53c3
hummayde31 <62832964+hummayde31@users.noreply.github.com>
2020-04-27 16:45:04 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -4,4 +4,14 @@ public class OperatorBinary {
     String nama = "Muhammad Humaidi";
     int jumlahSks = 24;
     double indeksPrestasikumulatif = 3.89;
+
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
 }
